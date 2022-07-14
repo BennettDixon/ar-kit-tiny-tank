f4be636e2d2bd479e2843ff6505ddc8732b6ca26
Bennett Dixon <benbendixon@gmail.com>
2022-07-14 08:37:00 -0600

    finished tiny toy tank ar project

--- TinyToyTank/ContentView.swift
+++ TinyToyTank/ContentView.swift
@@ -18,47 +18,69 @@ enum TankAction: String {
 class ActionDelegate: ObservableObject {
     static var shared: ActionDelegate = ActionDelegate()
     @Published var arView: ARView!
+    @Published var isActionPlaying: Bool = false
     
     func handleTankForwardPressed() {
-        //self.delegate.action = .tankForward
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
             anchor.notifications.tankForward.post()
         }
     }
     
     func handleTankLeftPressed() {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
-            anchor.notifications.tankForward.post()
+            anchor.notifications.tankLeft.post()
         }
     }
     
     func handleTankRightPressed() {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
-            anchor.notifications.tankForward.post()
+            anchor.notifications.tankRight.post()
         }
     }
     
     func handleFireTurretPressed() {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
-            anchor.notifications.tankForward.post()
+            anchor.notifications.cannonFire.post()
         }
     }
     
     func handleTurretRightPressed() {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
-            anchor.notifications.tankForward.post()
+            anchor.notifications.turretRight.post()
         }
     }
     
     func handleTurretLeftPressed() {
+        if self.isActionPlaying { return }
+        else { self.isActionPlaying = true }
+        
         if let anchor = arView.scene.anchors[0] as? TinyToyTank._TinyToyTank {
-            anchor.notifications.tankForward.post()
+            anchor.notifications.turretLeft.post()
         }
     }
     
     init() {
         arView = ARView(frame: .zero)
         let tankAnchor = try! TinyToyTank.load_TinyToyTank()
+        tankAnchor.cannon?.setParent(tankAnchor.tank, preservingWorldTransform: true)
+        tankAnchor.actions.actionComplete.onAction = { _ in
+          self.isActionPlaying = false
+        }
         arView.scene.anchors.append(tankAnchor)
     }
 }
